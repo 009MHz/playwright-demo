0c0bf2cc1b3f57b3291ab346d7b9f443d53aa72c
009MHz <mharisbsatriyo@gmail.com>
2024-12-13 13:12:19 +0700

    feat: initial login page completion

--- pages/login/login_page.py
+++ pages/login/login_page.py
@@ -17,28 +17,49 @@ class LogInPageInteraction(BasePage):
         assert r'/login' in self.page.url, f"Incorrect URL: {self.page.url} detected"
 
     async def username_insert(self, username: str):
-        pass
+        await expect(self._find(Interactor.username_input)).to_be_empty()
+        await self._type(Interactor.username_input, username)
+        await expect(self._find(Interactor.username_input)).not_to_be_empty()
 
     async def password_insert(self, password: str):
-        pass
+        await expect(self._find(Interactor.password_input)).to_be_empty()
+        await self._type(Interactor.password_input, password)
+        await expect(self._find(Interactor.password_input)).not_to_be_empty("")
 
     async def click_login_btn(self):
-        pass
+        await self._click(Interactor.login_btn)
 
 
 class LoginPageValidation(BasePage):
     async def header_presence(self):
-        pass
+        await self._look(PageInfo.header)
+        page_header = await self._find(PageInfo.header).text_content()
+        assert page_header == "Login Page", f"The Current page title:{page_header} not match with 'Login Page'"
 
     async def subheader_presence(self):
-        pass
+        await self._look(PageInfo.sub_header)
+        await expect(self._find(PageInfo.sub_header)).to_contain_text(
+            "This is where you can log into the secure area")
+        await expect(self._find(PageInfo.sub_header)).to_contain_text(
+            "If the information is wrong you should see error messages")
 
     async def username_field(self):
-        pass
+        await self._look(Interactor.username_label)
+        await expect(self._find(Interactor.username_label)).to_have_text("Username")
+        
+        await self._look(Interactor.username_input)
+        await expect(self._find(Interactor.username_input)).to_be_enabled()
+        await expect(self._find(Interactor.username_input)).to_have_value('')
 
     async def password_field(self):
-        pass
+        await self._look(Interactor.password_label)
+        await expect(self._find(Interactor.password_label)).to_have_text("Password")
+
+        await self._look(Interactor.password_input)
+        await expect(self._find(Interactor.password_input)).to_be_enabled()
+        await expect(self._find(Interactor.password_input)).to_have_value('')
 
     async def login_button(self):
-        pass
-
+        await self._look(Interactor.login_btn)
+        await expect(self._find(Interactor.login_btn)).to_be_enabled()
+        await expect(self._find(Interactor.login_btn)).to_contain_text("Login")
