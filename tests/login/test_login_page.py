b2f797d0b5f8846b6e9f803a17dc0e4519fcbae6
009MHz <mharisbsatriyo@gmail.com>
2024-12-13 13:12:48 +0700

    test: initial login page completion

--- tests/login/test_login_page.py
+++ tests/login/test_login_page.py
@@ -15,17 +15,35 @@ async def login(page):
 @allure.epic("Login Page")
 @allure.story("Login Page - Unit Test")
 @allure.feature("Login Page")
-@pytest.mark.positive
 @pytest.mark.smoke
-class TestSmokeJobPage:
-    @allure.title("Login Page Initial Test")
+class TestLoginInitPage:
+    @pytest.mark.positive
+    @allure.title("Login Page Initial State Check")
     @allure.severity(severity.CRITICAL)
     async def test_login_initial_state(self, login):
+        allure.dynamic.severity(severity.NORMAL)
         with allure.step("Check Initial Header existence"):
-            await login.check.header_existence()
+            await login.check.header_presence()
             await login.check.subheader_presence()
 
+        allure.dynamic.severity(severity.BLOCKER)
         with allure.step("Check Initial main field existence"):
             await login.check.username_field()
             await login.check.password_field()
             await login.check.login_button()
+
+    @pytest.mark.positive
+    @allure.title("Normal Login Page Action Flow")
+    @allure.severity(severity.CRITICAL)
+    async def test_login_flow_action(self, login):
+        with allure.step("1. Insert a valid username on the username field"):
+            await login.action.username_insert("tomsmith")
+
+        with allure.step("2. Insert a valid password on the password field"):
+            await login.action.password_insert("SuperSecretPassword!")
+
+        with allure.step("3. Click on the Login Button"):
+            await login.action.click_login_btn()
+
+        # with allure.step("4. Verify the success login state"):
+        #     await login.check.success_login()
