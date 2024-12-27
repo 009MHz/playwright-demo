08a39dec73d14b8c37ab536be22a9f5b342f754a
009MHz <mharisbsatriyo@gmail.com>
2024-12-28 01:37:24 +0700

    style: re indent scripts

--- pages/login/loginPage.ts
+++ pages/login/loginPage.ts
@@ -4,119 +4,119 @@ import { expect, Page } from '@playwright/test';
 
 export class LoginPage extends BasePage {
   constructor(page: Page) {
-      super(page);
+    super(page);
   }
 
 
   // Login Page Interaction
   async openPage() {
-      await this.page.goto(Url.login);
-      expect(this.page.url).toContain('/login');
+    await this.page.goto(Url.login);
+    expect(this.page.url).toContain('/login');
   }
 
   async usernameInsert(username: string) {
-      expect(await this._find(Interactor.username_input).isEditable()).toBeTruthy();
-      await this._type(Interactor.username_input, username);
-      await expect(this._find(Interactor.username_input)).toBeEditable();
-      await expect(this._find(Interactor.username_input)).not.toBeEmpty();
-      await this._capture(`Username Insert: ${username}`);
+    expect(await this._find(Interactor.username_input).isEditable()).toBeTruthy();
+    await this._type(Interactor.username_input, username);
+    await expect(this._find(Interactor.username_input)).toBeEditable();
+    await expect(this._find(Interactor.username_input)).not.toBeEmpty();
+    await this._capture(`Username Insert: ${username}`);
   }
 
   async passwordInsert(password: string) {
-      expect(await this._find(Interactor.password_input).isVisible()).toBeTruthy();
-      await this._type(Interactor.password_input, password);
-      await this._capture(`Password Insert: ${password}`);
+    expect(await this._find(Interactor.password_input).isVisible()).toBeTruthy();
+    await this._type(Interactor.password_input, password);
+    await this._capture(`Password Insert: ${password}`);
   }
 
   async clickLoginBtn() {
-      await this._click(Interactor.login_btn);
+    await this._click(Interactor.login_btn);
   }
 
   async closeSuccessBanner() {
-      await expect(this._find(PageInfo.banner_main)).toContainText('You logged out of the secure area!');
-      await this._click(PageInfo.banner_close);
-      await expect(this._find(PageInfo.banner_main)).not.toBeVisible();
+    await expect(this._find(PageInfo.banner_main)).toContainText('You logged out of the secure area!');
+    await this._click(PageInfo.banner_close);
+    await expect(this._find(PageInfo.banner_main)).not.toBeVisible();
   }
 
-  async closeInvalidUserBanner(){
+  async closeInvalidUserBanner() {
     await expect(this._find(PageInfo.banner_main)).toContainText('username is invalid!');
     await this._click(PageInfo.banner_close);
     await expect(this._find(PageInfo.banner_main)).not.toBeVisible();
   }
 
-  async closeInvalidPasswordBanner(){
+  async closeInvalidPasswordBanner() {
     await expect(this._find(PageInfo.banner_main)).toContainText('password is invalid!');
     await this._click(PageInfo.banner_close);
     await expect(this._find(PageInfo.banner_main)).not.toBeVisible();
   }
 
   // Login Page Validation
-async urlRedirection(){
-  expect (this.page.url).toContainEqual('/login');
-}
+  async urlRedirection() {
+    expect(this.page.url).toContainEqual('/login');
+  }
 
-async HeaderPresence(){
-  await this._look(PageInfo.header)
-  const page_header = await this._find(PageInfo.header).textContent();
-  expect(page_header).toBe("Login Page");
-}
+  async HeaderPresence() {
+    await this._look(PageInfo.header)
+    const page_header = await this._find(PageInfo.header).textContent();
+    expect(page_header).toBe("Login Page");
+  }
 
-async SubheaderPresence(){
-  await this._look(PageInfo.sub_header)
-  await expect(this._find(PageInfo.sub_header)).toContainText(
+  async SubheaderPresence() {
+    await this._look(PageInfo.sub_header)
+    await expect(this._find(PageInfo.sub_header)).toContainText(
       "This is where you can log into the secure area");
-  await expect(this._find(PageInfo.sub_header)).toContainText(
+    await expect(this._find(PageInfo.sub_header)).toContainText(
       "If the information is wrong you should see error messages");
-}
+  }
 
-async usernameFieldPresence(){
-  await this._look(Interactor.username_label)
-  await expect(this._find(Interactor.username_label)).toHaveText("Username")
-  
-  await this._look(Interactor.username_input)
-  await expect(this._find(Interactor.username_input)).toBeEnabled()
-  await expect(this._find(Interactor.username_input)).toBeEmpty()
-}
+  async usernameFieldPresence() {
+    await this._look(Interactor.username_label)
+    await expect(this._find(Interactor.username_label)).toHaveText("Username")
 
-async passwordFieldPresence(){
-  await this._look(Interactor.password_label)
-  await expect(this._find(Interactor.password_label)).toHaveText("Password")
+    await this._look(Interactor.username_input)
+    await expect(this._find(Interactor.username_input)).toBeEnabled()
+    await expect(this._find(Interactor.username_input)).toBeEmpty()
+  }
 
-  await this._look(Interactor.password_input)
-  await expect(this._find(Interactor.password_input)).toBeEnabled()
-  await expect(this._find(Interactor.password_input)).toBeEmpty()
-}
+  async passwordFieldPresence() {
+    await this._look(Interactor.password_label)
+    await expect(this._find(Interactor.password_label)).toHaveText("Password")
 
-//Login Page Validation: Successful Banner
-async loginButtonPresence(){
-  await this._look(Interactor.login_btn)
-  await expect(this._find(Interactor.login_btn)).toBeEnabled()
-  await expect(this._find(Interactor.login_btn)).toContainText("Login")
-}
+    await this._look(Interactor.password_input)
+    await expect(this._find(Interactor.password_input)).toBeEnabled()
+    await expect(this._find(Interactor.password_input)).toBeEmpty()
+  }
 
-async logoutBannerPresence(){
-  await this._look(PageInfo.banner_main)
-  await expect(this._find(PageInfo.banner_main)).toContainText("You logged out of the secure area!")
-  await this._capture("Logout Success Banner")
-}
+  //Login Page Validation: Successful Banner
+  async loginButtonPresence() {
+    await this._look(Interactor.login_btn)
+    await expect(this._find(Interactor.login_btn)).toBeEnabled()
+    await expect(this._find(Interactor.login_btn)).toContainText("Login")
+  }
 
-async logoutCloseBannerPresence(){
-  await this._look(PageInfo.banner_close)
-  await expect(this._find(PageInfo.banner_close)).toBeEnabled()
-}
+  async logoutBannerPresence() {
+    await this._look(PageInfo.banner_main)
+    await expect(this._find(PageInfo.banner_main)).toContainText("You logged out of the secure area!")
+    await this._capture("Logout Success Banner")
+  }
 
-// Login Page Validation: Invalid Banner
-async invalidBannerUsernamePresence(){
-  await this._look(PageInfo.banner_main)
-  await expect(this._find(PageInfo.banner_main)).toContainText("username is invalid!")
-  await expect(this._find(PageInfo.banner_close)).toBeEnabled()
-  await this._capture("Invalid Username Banner")
-}
+  async logoutCloseBannerPresence() {
+    await this._look(PageInfo.banner_close)
+    await expect(this._find(PageInfo.banner_close)).toBeEnabled()
+  }
 
-async invalid_banner_password(){
-  await this._look(PageInfo.banner_main)
-  await expect(this._find(PageInfo.banner_main)).toContainText("password is invalid!")
-  await expect(this._find(PageInfo.banner_close)).toBeEnabled()
-  await this._capture("Invalid Password Banner")
-}
+  // Login Page Validation: Invalid Banner
+  async invalidBannerUsernamePresence() {
+    await this._look(PageInfo.banner_main)
+    await expect(this._find(PageInfo.banner_main)).toContainText("username is invalid!")
+    await expect(this._find(PageInfo.banner_close)).toBeEnabled()
+    await this._capture("Invalid Username Banner")
+  }
+
+  async invalid_banner_password() {
+    await this._look(PageInfo.banner_main)
+    await expect(this._find(PageInfo.banner_main)).toContainText("password is invalid!")
+    await expect(this._find(PageInfo.banner_close)).toBeEnabled()
+    await this._capture("Invalid Password Banner")
+  }
 }
--- pages/login/preCond.ts
+++ pages/login/preCond.ts
@@ -3,20 +3,20 @@ import { Url, Interactor } from '../../elements/__login';
 import { Page, expect } from '@playwright/test';
 
 export class PreCond extends BasePage {
-    constructor(page: Page) {
-        super(page);
-    }
+  constructor(page: Page) {
+    super(page);
+  }
 
-    async loadLoginPage() {
-        await this.page.goto(Url.login);
-        expect(this.page.url()).toContain('/login');
-    }
+  async loadLoginPage() {
+    await this.page.goto(Url.login);
+    expect(this.page.url()).toContain('/login');
+  }
 
-    async loadSuccessPage(username: string, password: string) {
-        await this.loadLoginPage();
-        await this._type(Interactor.username_input, username);
-        await this._type(Interactor.password_input, password);
-        await this._click(Interactor.login_btn);
-        expect(this.page.url()).toContain('secure');
-    }
+  async loadSuccessPage(username: string, password: string) {
+    await this.loadLoginPage();
+    await this._type(Interactor.username_input, username);
+    await this._type(Interactor.password_input, password);
+    await this._click(Interactor.login_btn);
+    expect(this.page.url()).toContain('secure');
+  }
 }
