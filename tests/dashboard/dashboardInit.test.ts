3b3e439bb1c01c2516f4ee1103010b7775416892
009MHz <mharisbsatriyo@gmail.com>
2025-01-27 13:34:37 +0700

    chore: change import lib caller pattern follows tsconfig

--- tests/dashboard/dashboardInit.test.ts
+++ tests/dashboard/dashboardInit.test.ts
@@ -1,5 +1,5 @@
-import { test } from '../../src/utils/fixtures'; // Import the custom test object
-import { DashboardPage } from "../../src/pages/dashboard/DashboardPage";
+import { test } from '@utils/fixtures'; // Import the custom test object
+import { DashboardPage } from "@pages/dashboard/DashboardPage";
 
 test('Validate dashboard access after login', async ({ page }) => {
   let dashboardPage = new DashboardPage(page);
